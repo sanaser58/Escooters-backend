763d8c2fb467476e46be1e015ffe8eb71b211e34
sayednaser58 <sanaser9876@gmail.com>
2020-12-09 01:55:03 -0500

    update the ordercontroller and order routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,5 +75,14 @@ const getUserProfile = asyncHandler(async (req, res) => {
       throw new Error('User not found')
     }
   })
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
   
-  export { authUser, registerUser, getUserProfile }
+  export { authUser, registerUser, getUserProfile, getUsers }
